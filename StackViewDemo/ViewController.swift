0ace26744faccb21d757e018d9824db6238962c5
mlch911 <mlch911@mluoc.tech>
2017-03-28 02:52:34 +0800

    Version 0.01

---
+++ StackViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by 闵罗琛 on 2017/3/13.
+//  Copyright © 2017年 闵罗琛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
